e61dea5b80127f2b1951102dec9f04f64a6ec090
eshah126 <111527479+eshah126@users.noreply.github.com>
2023-09-15 14:14:15 -0500

    done

--- Chapter 15 Activities/Downsize/src/Business.java
+++ Chapter 15 Activities/Downsize/src/Business.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Business utility methods.
@@ -13,6 +14,25 @@ public class Business
      */
     public static void downsize(LinkedList<String> employeeNames, int n)
     {
-        ...
+        ListIterator <String> iterator = employeeNames.listIterator();
+        int counter = 0;
+
+        while(iterator.hasNext()){
+            if( counter == n-1){
+                iterator.n
+
+                iterator.remove();
+                counter = 0;
+            }
+            counter++;
+            }
+
+        System.out.print(employeeNames);
+
+
+
+
+
+
     }
 }
