6a8224e88067b57ba373ae923cbbdb63d2cd3487
eshah126 <111527479+eshah126@users.noreply.github.com>
2023-09-28 14:20:11 -0500

    dghfgh

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-. . .
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -9,8 +12,9 @@ public class Gradebook
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        in.nextLine();
 
-        . . .
+        Map<String, String> gradebook = new HashMap<>();
 
         boolean done = false;
         while(!done)
@@ -20,19 +24,45 @@ public class Gradebook
             if (input.equals("Q"))
             {
                 done = true;
+
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("What is the student's name");
+                String name = in.nextLine();
+                System.out.println();
+                System.out.println("What is the student's grade");
+                String grade = in.nextLine();
+                gradebook.put(name, grade);
+                
 
             } else if (input.equals("R"))
+
             {
-                . . .
+                System.out.println("What is the student's name");
+                String name = in.nextLine();
+                
+                //String grade = in.nextLine();
+                gradebook.remove(name);
+                
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("What students grade do you want to modify?");
+                String name = in.nextLine();
+                System.out.println("What is the student's  new grade?");
+                String Newgrade = in.nextLine();
+                gradebook.put(name, Newgrade);
+
+
+
+
+
+                
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> keys = gradebook.keySet();
+                for(String key : keys){
+                    System.out.println(key + gradebook.get(key));
+                }
             } else
             {
                 done = true;
--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,27 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /*THE MAP INTERFACE IS GENERIC
+         * THE FIRST TYPE IS THE KEY
+         * THE SECOND TYPE OF THE VALUE
+         */
+            Map<String, Color> favColors = new HashMap<>();
+            favColors.put("Peter", Color.BLACK);
+            favColors.put("Seth",Color.BLUE);
+            favColors.put("Celeste",Color.ORANGE);
+            favColors.put("Dr.Millers",Color.ORANGE);//2 different keys can have the same value
+
+            //The same key CANNOT HAVE 2 different values
+            favColors.put("Peter", Color.YELLOW);//replace
+
+
+
+            //create a set of the keys  in a map 
+            Set<String> keys = favColors.keySet();
+
+            for(String key : keys){
+                System.out.println(key + "("+key.hashCode()+"): " + favColors.get(key));
+            }
+
     }
 }
